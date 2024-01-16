2884ae5e36e634e125e87c97aa07096f1cabf454
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2024-01-16 09:03:39 -0500

    Añadiendo boton ver proyectos

--- script.js
+++ script.js
@@ -30,3 +30,37 @@ document.getElementById('addSkillContac').addEventListener('click', function() {
             alert("Gracias  "+newSkill+"  me pondré en contacto contigo pronto");
     }
 });
+
+const certificaciones = ["Certificacion Itil V4 PeopleSoft", "Srum Fundation", "SqlServer 2019", "Devops"];
+
+function mostrarCertificaciones() {
+    let ul = document.getElementById('listaCertificaciones'); // Suponiendo que tienes un <ul id="listaCertificaciones"> en tu HTML
+    
+    for (let i = 0; i < certificaciones.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = certificaciones[i];
+        ul.appendChild(li);
+    }
+}
+
+window.onload = mostrarCertificaciones;
+
+document.getElementById('viewProyects').addEventListener('click', function() {
+    var proyectos = [
+        "Hoja de vida ",
+        "Tienda de barrio ",
+        "Reservas",
+        "Enum",
+        "Polimorfismo"
+    ];
+var cantidadProyectos = prompt("Ingrese la cantidad de proyectos que desea ver:");
+    cantidadProyectos = parseInt(cantidadProyectos);
+    if (!isNaN(cantidadProyectos) && cantidadProyectos > 0) {
+        for (var i = 0; i < cantidadProyectos; i++) {
+            var indice = i % proyectos.length;
+            alert("Proyecto " + (i + 1) + ": " + proyectos[indice]);
+        }
+    } else {
+        alert("El numero que ingreso debe ser mayor a 0.");
+    }
+});
